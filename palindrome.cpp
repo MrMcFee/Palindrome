d29478795242be3f901cd753434fe28b1987efad
MrMcFee <55999100+MrMcFee@users.noreply.github.com>
2020-09-28 09:23:38 -0700

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main() {
+//Definitions/inputs
   char string[81];
   char string2[81];
   char string3[81];
@@ -11,6 +12,7 @@ int main() {
   cin.get();
   int i, j;
   int length = strlen(string);
+//Walks through the input and removes punctuation, spaces, and caps and puts into string2.
   for (i = 0, j = 0; i < length; i++) {
     if ((string[i] != ' ') && (ispunct(string[i]) == false)) {
       string2[j] = tolower(string[i]);
@@ -18,11 +20,14 @@ int main() {
     }
   }
   strcpy(string, string2);
-  cout << string << endl;
-  cout << string2 << endl;
+//flips char array around, outputs result to line.
   for (int k = length - 1; k >= 0; k--) {
     cout << string2[k];
+//This is where my code messes up. When comparing string and string 2, it takes the original string array and the original string2
+//array and compares them, rather than the string2[k] flipped array, and I don't really know how to get it into the compare function.
+//The function itself works fine, I just don't know how to get the correct array in.
     int compare = strcmp(string, string2);
+//When it reaches the final char, it compares the flipped array and regular array and tells you if it's a palindrome or not
     if (k == 0) { 
       if (compare == 0) {
 	cout << endl;
